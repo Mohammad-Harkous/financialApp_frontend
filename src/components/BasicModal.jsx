7a3d41fd1a243450ea96d6c7a8e16d5a80d0f9d2
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2023-03-13 03:01:56 +0200

    added post and deleterequest

--- src/components/BasicModal.jsx
+++ src/components/BasicModal.jsx
@@ -1,9 +1,13 @@
 import { useState} from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import CustomButton from './CustomButton';
+import { TextField } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
 const style = {
   position: 'absolute',
@@ -17,10 +21,53 @@ const style = {
   p: 4,
 };
 
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     position: 'absolute',
+//     top: '50%',
+//     left: '50%',
+//     transform: 'translate(-50%, -50%)',
+//     width: 400,
+//     backgroundColor: theme.palette.background.paper,
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
+
+
+
 function BasicModal() {
   const [open, setOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    full_name:'',
+    email:'',
+    password:'',
+    is_admin: false
+  });
+  
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+  
+    
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/api/register', formData,{
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      // handle successful response
+      console.log(response.data)
+     
+    } catch (error) {
+      // handle error
+      console.log(error);
+    }}
+
 
   return (
     <div>
@@ -31,13 +78,26 @@ function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-          <Button onClick={handleClose}>Close modal</Button>
+        <IconButton sx={{ml:45}}>
+          <CloseIcon onClick={handleClose}/> 
+        </IconButton>
+        <form onSubmit={handleSubmit} >
+        <label>Full Name</label>
+        <TextField type ="text" name='full_name'  label="Full Name" fullWidth margin="normal" value={formData.full_name} onChange={(e) => setFormData({ ...formData, full_name: e.target.value })}/>
+
+        <label htmlFor="Email">Email</label>
+        <TextField type ="text" name='email' label="Email" fullWidth margin="normal" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })}/>
+
+        <label htmlFor="Password">Password</label>
+        <TextField type ="password" name='password'  label="Password" fullWidth margin="normal" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })}/>
+
+         {/* <label htmlFor="is_admin">IsAdmin</label>
+        <TextField  type="checkbox" label="IsAdmin" fullWidth margin="normal" checked={formData.is_admin} 
+         onChange={(e) => setFormData({ ...formData, is_admin: e.target.value })}/>  */}
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </form>
         </Box>
       </Modal>
     </div>
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import BasicModal from '../components/BasicModal'
+import BasicModal from '../components/BasicModal';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
@@ -16,13 +16,19 @@ function Users() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  useEffect(() => {
-    async function fetchData() {
-      const result = await axios.get('http://127.0.0.1:8000/api/users');
+  const fetchUsers = async () => {
+    try{
+    const result = await axios.get('http://127.0.0.1:8000/api/users');
       setData(result.data);
-      console.log(result.data);
+     console.log(result.data);
+    }catch(error){
+      console.log(error);
     }
-    fetchData();
+  }
+
+
+  useEffect(() => {
+    fetchUsers();
   }, []);
 
   const handleChangePage = (event, newPage) => {
@@ -36,7 +42,18 @@ function Users() {
 
 
   function onEdit(){}
-  function onDelete(){}
+
+  const onDelete = async (id) =>{
+
+     try{
+        await axios.delete(`http://127.0.0.1:8000/api/users/${id}`)
+        console.log(`User ${id} has been deleted`);
+        fetchUsers();
+    }catch (error) {
+      console.error(`Error deleting User${id}: ${error}`);
+    }
+
+  }
 
   return (
     <Box sx={{ml:40, mt:3}}>
@@ -44,7 +61,7 @@ function Users() {
       <Paper variant="outlined" >
       <BasicModal />
       </Paper>
-      <TableContainer component={Paper}>
+      <TableContainer component={Paper} >
       <Table>
         <TableHead>
           <TableRow>
@@ -62,10 +79,10 @@ function Users() {
               <TableCell>{user.email}</TableCell>
               
               <TableCell>
-                <IconButton onClick={() => onEdit(user)}>
+                <IconButton onClick={() => onEdit(user.id)}>
                   <EditIcon />
                 </IconButton>
-                <IconButton onClick={() => onDelete(user)}>
+                <IconButton onClick={() => onDelete(user.id)}>
                   <DeleteIcon />
                 </IconButton>
               </TableCell>
